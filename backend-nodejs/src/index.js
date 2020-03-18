2dc8bea53d8055fb9641474d56755d96d58afa94
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-03-18 18:19:35 -0300

    JWT Middleware

--- backend-nodejs/src/index.js
+++ backend-nodejs/src/index.js
@@ -7,5 +7,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
 require('./controllers/authController')(app);
+require('./controllers/habitController')(app);
 
 app.listen(8080);
---
+++ backend-nodejs/src/middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    //simple verifications for performance improvement
+
+    if (!authHeader)
+        return res.status(401).send({ error: 'No token provided' });
+
+    const tokenBody = authHeader.split(' ');
+
+    if (tokenBody.length != 2)
+        return res.status(401).send({ error: 'Token malformatted' });
+
+    const [scheme, token] = tokenBody;
+
+    if (!/^Berear$/i.test(scheme))
+        return res.status(401).send({ error: 'Token malformatted' });
+
+    jwt.verify(token, authConfig.secret, (err, decoded) =>
+    {
+        if(err) 
+            return res.status(401).send({error:'Invalid token'});
+
+        req.userId = decoded.id;
+        return next();
+    });
+};
