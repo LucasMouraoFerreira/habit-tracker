f2b9ab51d3c9484ae8b7c8efd5169a45155474f1
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-03-18 17:46:57 -0300

    JWT dependency and token generation

--- backend-nodejs/src/controllers/authController.js
+++ backend-nodejs/src/controllers/authController.js
@@ -1,18 +1,60 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const User = require("../models/user");
+const authConfig = require('../config/auth')
+
+const User = require('../models/user');
+
 
 const router = express.Router();
 
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+    });
+}
+
 router.post('/register', async (req, res) => {
+
+    const { email } = req.body;
+
     try {
+
+        if (await User.findOne({ email }))
+            return res.status(400).send({ error: 'Email is already being used' })
+
         const user = await User.create(req.body);
 
-        return res.send({ user });
+        user.password = undefined;
+
+        return res.send({ 
+            user,
+            token: generateToken({id: user.id}),
+        });
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
 });
 
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('password').select('name').select('email');
+
+    if (!user)
+        return res.status(404).send({ error: 'User not found' });
+
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Invalid password' });
+
+    user.password = undefined;
+
+    res.send({ 
+        user, 
+        token: generateToken({id: user.id}),
+     });
+});
 
 module.exports = app => app.use('/auth', router);
+
--- backend-nodejs/src/models/user.js
+++ backend-nodejs/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -16,6 +17,13 @@ const UserSchema = new mongoose.Schema({
         required: true,
         select: false,
     },
+});
+
+UserSchema.pre('save', async function (next) {
+    const hash = await bcrypt.hash(this.password, 12);
+    this.password = hash;
+
+    next();
 });
 
 const User = mongoose.model('User', UserSchema);
