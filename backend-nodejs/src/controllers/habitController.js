d1bb1a466c23dc9acbd58043773cd3f4298414b8
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-03-18 22:42:46 -0300

    habitController - get, post

--- backend-nodejs/src/controllers/habitController.js
+++ backend-nodejs/src/controllers/habitController.js
@@ -8,11 +8,27 @@ const router = express.Router();
 router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
-    res.send({ user: req.userId });
+
+    try {
+        const habits = await Habit.find({user: req.userId});
+
+        return res.send(habits);
+    } catch (err) {
+        return res.status(400).send({ error: 'Error loading habits' });
+    }
 });
 
 router.post('/', async (req, res) => {
-    res.send({ user: req.userId })
+
+    try {
+        const habit = await Habit.create({...req.body, user: req.userId});
+
+        return res.send({ habit });
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Error creating habit'});
+    }
+  
 });
 
 router.put('/:habitId', async (req, res) => {
