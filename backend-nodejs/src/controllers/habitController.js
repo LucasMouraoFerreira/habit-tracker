85bc8ab1fc267c38f28d5feda6de5c1171dca7c3
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-03-18 23:38:56 -0300

    habitController - update, delete

--- backend-nodejs/src/controllers/habitController.js
+++ backend-nodejs/src/controllers/habitController.js
@@ -10,7 +10,7 @@ router.use(authMiddleware);
 router.get('/', async (req, res) => {
 
     try {
-        const habits = await Habit.find({user: req.userId});
+        const habits = await Habit.find({ user: req.userId });
 
         return res.send(habits);
     } catch (err) {
@@ -20,22 +20,42 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
 
+    const { name, reminderMessage, frequency, color } = req.body;
+
     try {
-        const habit = await Habit.create({...req.body, user: req.userId});
+        const habit = await Habit.create({ name, reminderMessage, frequency, color, user: req.userId });
 
         return res.send({ habit });
 
     } catch (err) {
-        return res.status(400).send({ error: 'Error creating habit'});
+        return res.status(400).send({ error: 'Error creating habit' });
     }
-  
+
 });
 
 router.put('/:habitId', async (req, res) => {
-    res.send({ user: req.userId });
+
+    const { name, reminderMessage, frequency, color } = req.body;
+
+    try {
+        const habit = await Habit.findByIdAndUpdate(req.params.habitId, { name, reminderMessage, frequency, color }, {new : true});
+
+        return res.send({ habit });
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Error updating habit' });
+    }
+
 });
 
 router.delete('/:habitId', async (req, res) => {
+    try {
+        await Habit.findByIdAndDelete(req.params.habitId);
+
+        res.status(204).send();
+    } catch (err) {
+        return res.status(400).send({ error: 'Error deleting habit' });
+    }
     res.send({ user: req.userId });
 });
 
